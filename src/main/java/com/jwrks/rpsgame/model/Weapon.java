f9d7bde29458ea35c90622e8c47380fdcd53f900
ikscn <ilker.kscn@gmail.com>
2024-01-23 16:10:11 +0100

    Fixed Weapon constructor

--- src/main/java/com/jwrks/rpsgame/model/Weapon.java
+++ src/main/java/com/jwrks/rpsgame/model/Weapon.java
@@ -3,10 +3,12 @@ package com.jwrks.rpsgame.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Weapon {
 
     @Id
